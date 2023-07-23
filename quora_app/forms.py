e75afe5c47d7ae5fbd064d9f688cd6fe74ee2bc4
Rushikesh8 <rushikeshsakharwade@gmail.com>
2023-07-23 15:42:11 +0530

    remove comment

--- quora_app/forms.py
+++ quora_app/forms.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 from .models import Question,Answer
 
 class CustomSignupForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 class CustomLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
 
 class QuestionForm(forms.ModelForm):
